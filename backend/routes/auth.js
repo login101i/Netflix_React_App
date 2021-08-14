05849a3d74710c2bd247c7a47288adb228319863
McKrus <maciejkruszyniak@gmail.com>
2021-08-14 12:38:32 +0200

    register/login user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 // register
 
@@ -8,13 +8,29 @@ router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
   });
   try {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(401).json(err);
+  }
+});
+
+// login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Nieprawidłowe hasło lub email");
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+    originalPassword !== req.body.password && res.status(401).json("Nieprawidłowe hasło lub email koleżko");
+
+    const { password, ...info } = user._doc;
+    res.status(200).json(INFO );
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
